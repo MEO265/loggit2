bdefc3d0e135cd6c4ad387e1a1262ecd60e6428b
MEO265 <99362508+MEO265@users.noreply.github.com>
2024-01-08 20:19:09 +0100

    mnt: Remove `bind_rows_loggit`

--- R/utils.R
+++ R/utils.R
@@ -1,25 +1,3 @@
-# Helper function to append rows 
-bind_rows_loggit <- function(df1, df2) {
-  stopifnot('data.frame' %in% class(df1))
-  stopifnot('data.frame' %in% class(df2))
-  
-  headers1 <- colnames(df1)
-  headers2 <- colnames(df2)
-  headers <- union(headers1, headers2)
-  
-  for (header in headers) {
-    if (!(header %in% headers1)) {
-      df1[, header] <- NA
-    }
-    if (!(header %in% headers2)) {
-      df2[, header] <- NA
-    }
-  }
-  
-  rbind(df1, df2)
-}
-
-
 #' Return log file as an R data frame
 #'
 #' This function returns a `data.frame` containing all the logs in the provided
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,14 +1,3 @@
-test_that("bind_rows_loggit works", {
-  df1 <- data.frame(a = c(1, 2, 3))
-  df2 <- data.frame(b = c(1, 2, 3))
-  df_out <- data.frame(
-    a = c(1, 2, 3, NA, NA, NA),
-    b = c(NA, NA, NA, 1, 2, 3)
-  )
-  expect_equal(bind_rows_loggit(df1, df2), df_out)
-})
-
-
 test_that("rotate_logs works on default log file", {
   for (i in 1:100) {
     loggit("INFO", paste0("log_", i), echo = FALSE)
