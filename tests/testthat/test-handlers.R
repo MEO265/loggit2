f69061492acafd5926d1c755d5667b6be18343b3
MEO265 <99362508+MEO265@users.noreply.github.com>
2024-01-10 21:21:07 +0100

    tests: Add 3th tests for `stopifnot`

--- tests/testthat/test-handlers.R
+++ tests/testthat/test-handlers.R
@@ -1,7 +1,7 @@
 test_that("message works as it does in base R", {
   expect_message(base::message("this is a message test"))
   expect_message(loggit::message("this is also a message test", echo = FALSE))
-  
+
   # Multiple args are concatenated
   captured_output <- capture_output(
     loggit::message('this should be ', 'concatenated ', 'in the log')
@@ -13,7 +13,7 @@ test_that("message works as it does in base R", {
 test_that("warning works as it does in base R", {
   expect_warning(base::warning("this is a warning test"))
   expect_warning(loggit::warning("this is also a warning test", echo = FALSE))
-  
+
   # Multiple args are concatenated
   suppressWarnings(
     captured_output <- capture_output(
@@ -27,7 +27,7 @@ test_that("warning works as it does in base R", {
 test_that("stop works as it does in base R", {
   expect_error(base::stop("this is a stop test"))
   expect_error(loggit::stop("this is also a stop test", echo = FALSE))
-  
+
   # Multiple args are concatenated
   # Test looks different to get around the stop() call
   expect_error(loggit::stop('this should be ', 'concatenated ', 'in the log', echo = FALSE))
@@ -39,17 +39,34 @@ test_that("stop works as it does in base R", {
 
 cleanup()
 
-test_that("stopifnot",{
-  # stopifnot works as in base R
-  expect_identical_error(stopifnot(FALSE),base::stopifnot(FALSE))
-  f <- function (x, ...) x
+test_that("stopifnot", {
+  # stopifnot works as in base R (with echo = TRUE)
+  expect_identical_error(stopifnot(FALSE), base::stopifnot(FALSE))
+  f <- function(x, ...) x
   expect_identical_error(stopifnot(f(x = FALSE)), base::stopifnot(f(x = FALSE)))
-  g <- function () f(FALSE)
-  expect_identical_error(stopifnot(4 == 4, g()), base::stopifnot(4==4, g()))
-  expect_identical_error(stopifnot(4 == 4, "Test" = g()), base::stopifnot(4==4, "Test" = g()))
-  expect_identical_error(stopifnot(exprs = {TRUE; FALSE}), base::stopifnot(exprs = {TRUE; FALSE}))
-  expect_identical_error(stopifnot(exprObject = {TRUE; FALSE}), base::stopifnot(exprObject = {TRUE; FALSE}))
+  g <- function() f(FALSE)
+  expect_identical_error(stopifnot(4 == 4, g()), base::stopifnot(4 == 4, g()))
+  expect_identical_error(stopifnot(4 == 4, "Test" = g()), base::stopifnot(4 == 4, "Test" = g()))
+  expect_identical_error(stopifnot(exprs = { TRUE; FALSE }), base::stopifnot(exprs = { TRUE; FALSE }))
+  expect_identical_error(stopifnot(exprObject = { TRUE; FALSE }), base::stopifnot(exprObject = { TRUE; FALSE }))
   expect_no_error(stopifnot(TRUE, 3 == 3))
+
+  # stopifnot works as in base R (with echo = FALSE)
+  expect_identical_error(stopifnot(FALSE, echo = FALSE), base::stopifnot(FALSE))
+  f <- function(x, ...) x
+  expect_identical_error(stopifnot(f(x = FALSE), echo = FALSE), base::stopifnot(f(x = FALSE)))
+  g <- function() f(FALSE)
+  expect_identical_error(stopifnot(4 == 4, g(), echo = FALSE), base::stopifnot(4 == 4, g()))
+  expect_identical_error(stopifnot(4 == 4, "Test" = g(), echo = FALSE), base::stopifnot(4 == 4, "Test" = g()))
+  expect_identical_error(stopifnot(exprs = { TRUE; FALSE }, echo = FALSE), base::stopifnot(exprs = { TRUE; FALSE }))
+  expect_identical_error(stopifnot(exprObject = { TRUE; FALSE }, echo = FALSE), base::stopifnot(exprObject = { TRUE; FALSE }))
+  expect_no_error(stopifnot(TRUE, 3 == 3, echo = FALSE))
+
+  cleanup()
+
+  # Test for logging
+
+
 })
 
-cleanup()
+#cleanup()
