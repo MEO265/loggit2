8d26097210ceeee5764d18c230555222294c29e1
MEO265 <99362508+MEO265@users.noreply.github.com>
2024-02-18 14:08:29 +0100

    test: Test on old normalized path after reset II

--- tests/testthat/test-configurations.R
+++ tests/testthat/test-configurations.R
@@ -11,7 +11,7 @@ test_that("test logfile configuration", {
     set_logfile(old, confirm = TRUE),
     regexp = "^Log file set to .*\n$"
   )
-  expect_identical(get_logfile(), expected = normalizePath(old, mustWork = FALSE))
+  expect_identical(get_logfile(), expected = normalizePath(old, mustWork = FALSE, winslash = "/"))
 })
 
 cleanup()
