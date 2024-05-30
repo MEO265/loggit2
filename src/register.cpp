1cd9470f3e92e4b5162d2a0a55427c6047feaf7c
MEO265 <99362508+MEO265@users.noreply.github.com>
2024-05-30 06:48:53 +0200

    mnt: Fix registration

--- src/register.cpp
+++ src/register.cpp
@@ -1,14 +1,18 @@
 #include <R.h>
 #include <Rinternals.h>
 #include <R_ext/Rdynload.h>
+#include <R_ext/Visibility.h>
 #include "loggit.h"
 
+extern "C" {
 static const R_CallMethodDef CallEntries[] = {
-    {"split_ndjson", (DL_FUNC) &split_ndjson, 1},
+    {"c_split_ndjson", (DL_FUNC) &split_ndjson, 1},
     {NULL, NULL, 0}
 };
+}
 
-void R_init_loggit2(DllInfo *dll) {
-    R_useDynamicSymbols(dll, FALSE);
+extern "C" attribute_visible void R_init_loggit2(DllInfo *dll) {
     R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+    R_forceSymbols(dll, TRUE);
 }
